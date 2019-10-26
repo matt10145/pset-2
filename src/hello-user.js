25f5b8fcbe3025b59a44b479331a1fb2043d5bdc
matth101 <mhui@ucvts.org>
2019-10-26 19:28:58 -0400

    fix spacing

--- src/hello-user.js
+++ src/hello-user.js
@@ -1,5 +1,5 @@
 const readlineSync = require("readline-sync");
 
 const firstName = readlineSync.question("\nHi! What's your name?\n");
-console.log(" ");
+console.log("");
 console.log("Hello, " + firstName + "!");
